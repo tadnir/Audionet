0f82709f5d893edfb303608f41f5590ca1947fcd
Michael Tadnir <tadnir50@gmail.com>
2024-02-04 10:01:41 +0200

    Lorem Ipsum

--- src/asocket/asocket.c
+++ src/asocket/asocket.c
@@ -2,18 +2,19 @@
 #include <math.h>
 #include <stdint.h>
 #include <assert.h>
+#include <string.h>
 #include "asocket.h"
 
 #include "audio/audio.h"
 #include "logger.h"
 #include "fft/fft.h"
 
-#define BASE_CHANNEL_FREQUENCY (200)
-#define CHANNEL_FREQUENCY_BAND_WIDTH (50)
+#define BASE_CHANNEL_FREQUENCY (100)
+#define CHANNEL_FREQUENCY_BAND_WIDTH (150)
 #define NUMBER_OF_CHANNELS (13)
 #define NUMBER_OF_CONCURRENT_CHANNELS (3)
 #define AMPLITUDE_MAGNITUDE_THRESHOLD (0.1)
-#define SYMBOL_LENGTH_MILLISECONDS (200)
+#define SYMBOL_LENGTH_MILLISECONDS (150)
 
 #define FREQUENCY_TO_CHANNEL_INDEX(frequency) (                                                           \
     (ROUND_TO((frequency), CHANNEL_FREQUENCY_BAND_WIDTH) / CHANNEL_FREQUENCY_BAND_WIDTH)                  \
@@ -27,9 +28,31 @@
 )
 #define ROUND_TO(number, rounder) (((float)rounder) * floor((((float)number)/((float)rounder))+0.5))
 
+enum signals {
+
+    /* 256 byte values 0-255 */
+
+    SIGNAL_PREAMBLE = 270,
+    SIGNAL_SEP = 275,
+    SIGNAL_POST = 280,
+
+    RANGE_END = 285 /* inclusive */
+};
+
+enum state_e {
+    STATE_PREAMBLE,
+    STATE_WORD,
+    STATE_SEP,
+};
+
 struct asocket_s {
     audio_t* audio;
     fft_t* fft;
+    enum state_e state;
+    unsigned char votes[256];
+    bool voted;
+    char buffer[1024];
+    int index;
 };
 
 uint64_t comb(int n, int r) {
@@ -42,7 +65,7 @@ uint64_t comb(int n, int r) {
     return sum;
 }
 
-static uint64_t channels_to_int(int a, int b, int c) {
+static uint64_t channels_to_int(unsigned int a, unsigned int b, unsigned int c) {
 //    def channels_to_int(a, b, c):
 //        b=b-1-a
 //        c=c-2-a
@@ -97,6 +120,7 @@ static struct channels int_to_channels(uint64_t value) {
     }
 
     struct channels out = {a, b, c};
+    LOG_INFO("out: %d, %d, %d -> %llu (%c)", a, b, c, value, value == SIGNAL_PREAMBLE ? '^': value == SIGNAL_SEP ? '+': value == SIGNAL_POST ? '$' : (char)value);
     return out;
 }
 
@@ -141,27 +165,107 @@ static void listen_callback(asocket_t* socket, const float* recorded_frame, size
     qsort((void*)frequencies, count_frequencies, sizeof(struct frequency_and_magnitude),
           (int (*)(const void *, const void *)) compare_magnitudes);
 
-    /* If there aren't at least NUMBER_OF_CONCURRENT_CHANNELS frequencies with some noticeable sound, we consider it as quiet. */
+    /* If there aren't at least NUMBER_OF_CONCURRENT_CHANNELS frequencies with some noticeable sound,
+     * we consider it as quiet and there's no reason to continue. */
     if (frequencies[NUMBER_OF_CONCURRENT_CHANNELS - 1].magnitude <= AMPLITUDE_MAGNITUDE_THRESHOLD) {
-        LOG_INFO("Quiet");
         goto l_cleanup;
     }
 
-    int values[NUMBER_OF_CONCURRENT_CHANNELS];
-    for (int i = 0; i < NUMBER_OF_CONCURRENT_CHANNELS; i++) {
-        int frequency_value = FREQUENCY_TO_CHANNEL_INDEX(frequencies[i].frequency);
-        printf("%d (%.0fHz),  ", frequency_value, frequencies[i].frequency);
-        values[i] = frequency_value;
-    }
-    printf("\n");
+    unsigned int channels[NUMBER_OF_CONCURRENT_CHANNELS];
+    int channels_found = 0;
+    for (int i = 0; i < count_frequencies; i++) {
+        if (frequencies[i].magnitude <= AMPLITUDE_MAGNITUDE_THRESHOLD) {
+            LOG_DEBUG("Got to frequency index %d and couldn't find channel strong sounds", i);
+            break;
+        }
+        
+        unsigned int channel = FREQUENCY_TO_CHANNEL_INDEX(frequencies[i].frequency);
+        if (channel >= NUMBER_OF_CHANNELS) {
+//            LOG_DEBUG("Invalid channel number: %d (probably due to noise)", channel);
+            continue;
+        }
 
-    qsort(values, NUMBER_OF_CONCURRENT_CHANNELS, sizeof(int), compare_ints);
-    if (values[0] == values[1] || values[1] == values[2]) {
-        LOG_ERROR("Got two frequencies on the same channel");
+        bool is_new = true;
+        for (int j = 0; j < channels_found; ++j) {
+            if (channel == channels[j]) {
+//                LOG_DEBUG("Channel %d found twice, might be due to noise, multi-channel speaker or algo collision", channel);
+                is_new = false;
+            }
+        }
+
+        if (is_new) {
+            printf("%d (%.0fHz),  ", channel, frequencies[i].frequency);
+            channels[channels_found] = channel;
+            channels_found++;
+
+            if (channels_found == NUMBER_OF_CONCURRENT_CHANNELS) {
+                break;
+            }
+        }
+
+    }
+//    printf("\n");
+
+    /* Couldn't find enough channels */
+    if (channels_found < NUMBER_OF_CONCURRENT_CHANNELS) {
         goto l_cleanup;
     }
 
-    LOG_INFO("values: %d, %d, %d -> %lld", values[0], values[1], values[2], channels_to_int(values[0], values[1], values[2]));
+    qsort(channels, NUMBER_OF_CONCURRENT_CHANNELS, sizeof(int), compare_ints);
+    uint64_t value = channels_to_int(channels[0], channels[1], channels[2]);
+    LOG_INFO("values: %d, %d, %d -> %lld", channels[0], channels[1], channels[2], value);
+    switch (value) {
+        case 0 ... 255:
+            if (socket->state == STATE_WORD) {
+                socket->voted = true;
+                socket->votes[value]++;
+                for (int i = 0; i < 256; ++i) {
+                    if (socket->votes[i] != 0)
+                        printf("%d,\t", i);
+                }
+                printf("\n");
+                for (int i = 0; i < 256; ++i) {
+                    if (socket->votes[i] != 0)
+                        printf("%d,\t", socket->votes[i]);
+                }
+                printf("\n");
+            }
+            break;
+        case SIGNAL_PREAMBLE ... SIGNAL_SEP-1:
+            if (socket->state == STATE_PREAMBLE) {
+                socket->index = 0;
+                socket->state = STATE_WORD;
+            }
+            break;
+        case SIGNAL_SEP ... SIGNAL_POST - 1:
+            if (socket->state == STATE_WORD && socket->voted) {
+                socket->voted = false;
+                unsigned char max = 0;
+                for (int i = 0; i < 256; ++i) {
+                    if (socket->votes[i] > max) {
+                        max = (unsigned char)i;
+                    }
+                }
+                memset(socket->votes, 0, 256);
+                socket->buffer[socket->index] = max;
+                socket->index++;
+                LOG_DEBUG("data: %hhx (%c)", max, max);
+            }
+            break;
+        case SIGNAL_POST + 2 ... RANGE_END:
+            if (socket->state != STATE_PREAMBLE) {
+                //TODO: output the accumulated buffer.
+                socket->buffer[socket->index] = '\0';
+                printf("buffer: <%s>", socket->buffer);
+//                memset(socket->buffer, 0, 1024);
+//                socket->index = 0;
+                socket->state = STATE_PREAMBLE;
+            }
+            break;
+        default:
+            LOG_DEBUG("Unknown signal %llu", value);
+            break;
+    }
 
 l_cleanup:
     if (frequencies != NULL) {
@@ -176,6 +280,12 @@ asocket_t* ASOCKET__initialize() {
         LOG_ERROR("Failed to initialize Audio Socket");
         return NULL;
     }
+
+    socket->state = STATE_PREAMBLE;
+    socket->index = 0;
+    memset(socket->votes, 0, 256);
+    socket->voted = false;
+    memset(socket->buffer, 0, 1024);
 
     LOG_DEBUG("Initializing FFT");
     socket->fft = FFT__initialize(3600, SAMPLE_RATE_48000);
@@ -227,23 +337,45 @@ int ASOCKET__listen(asocket_t* socket) {
 
 int ASOCKET__send(asocket_t* socket, void* data, size_t size) {
     int status = -1;
+    struct channels channels;
 
-    struct sound* sounds_packet = malloc(size * sizeof(struct sound));
+    struct sound* sounds_packet = malloc((2 + 2 * size) * sizeof(struct sound));
     if (sounds_packet == NULL) {
         LOG_ERROR("Failed to allocate sounds packet");
         return -1;
     }
 
-    for (int i = 0; i < size; ++i) {
-        struct channels channels = int_to_channels(((uint8_t*)data)[i]);
-        sounds_packet[i].length_milliseconds = SYMBOL_LENGTH_MILLISECONDS;
-        sounds_packet[i].number_of_frequencies = NUMBER_OF_CONCURRENT_CHANNELS;
-        sounds_packet[i].frequencies[0] = CHANNEL_INDEX_TO_FREQUENCY(channels.a);
-        sounds_packet[i].frequencies[1] = CHANNEL_INDEX_TO_FREQUENCY(channels.b);
-        sounds_packet[i].frequencies[2] = CHANNEL_INDEX_TO_FREQUENCY(channels.c);
+    channels = int_to_channels(SIGNAL_PREAMBLE+1);
+    sounds_packet[0].length_milliseconds = SYMBOL_LENGTH_MILLISECONDS * 2;
+    sounds_packet[0].number_of_frequencies = NUMBER_OF_CONCURRENT_CHANNELS;
+    sounds_packet[0].frequencies[0] = CHANNEL_INDEX_TO_FREQUENCY(channels.a);
+    sounds_packet[0].frequencies[1] = CHANNEL_INDEX_TO_FREQUENCY(channels.b);
+    sounds_packet[0].frequencies[2] = CHANNEL_INDEX_TO_FREQUENCY(channels.c);
+
+    for (int i = 0; i < size; i++) {
+        channels = int_to_channels(((uint8_t*)data)[i]);
+        sounds_packet[1+i*2].length_milliseconds = SYMBOL_LENGTH_MILLISECONDS*2;
+        sounds_packet[1+i*2].number_of_frequencies = NUMBER_OF_CONCURRENT_CHANNELS;
+        sounds_packet[1+i*2].frequencies[0] = CHANNEL_INDEX_TO_FREQUENCY(channels.a);
+        sounds_packet[1+i*2].frequencies[1] = CHANNEL_INDEX_TO_FREQUENCY(channels.b);
+        sounds_packet[1+i*2].frequencies[2] = CHANNEL_INDEX_TO_FREQUENCY(channels.c);
+
+        channels = int_to_channels(SIGNAL_SEP+1);
+        sounds_packet[2+i*2].length_milliseconds = SYMBOL_LENGTH_MILLISECONDS;
+        sounds_packet[2+i*2].number_of_frequencies = NUMBER_OF_CONCURRENT_CHANNELS;
+        sounds_packet[2+i*2].frequencies[0] = CHANNEL_INDEX_TO_FREQUENCY(channels.a);
+        sounds_packet[2+i*2].frequencies[1] = CHANNEL_INDEX_TO_FREQUENCY(channels.b);
+        sounds_packet[2+i*2].frequencies[2] = CHANNEL_INDEX_TO_FREQUENCY(channels.c);
     }
 
-    status = AUDIO__play_sounds(socket->audio, sounds_packet, size);
+    channels = int_to_channels(SIGNAL_POST+2);
+    sounds_packet[1 + 2 * size].length_milliseconds = SYMBOL_LENGTH_MILLISECONDS*2;
+    sounds_packet[1 + 2 * size].number_of_frequencies = NUMBER_OF_CONCURRENT_CHANNELS;
+    sounds_packet[1 + 2 * size].frequencies[0] = CHANNEL_INDEX_TO_FREQUENCY(channels.a);
+    sounds_packet[1 + 2 * size].frequencies[1] = CHANNEL_INDEX_TO_FREQUENCY(channels.b);
+    sounds_packet[1 + 2 * size].frequencies[2] = CHANNEL_INDEX_TO_FREQUENCY(channels.c);
+
+    status = AUDIO__play_sounds(socket->audio, sounds_packet, 2+size*2);
     if (status != 0) {
         LOG_ERROR("Failed to play sounds");
         status = -1;
@@ -260,7 +392,14 @@ l_cleanup:
     return status;
 }
 
+
+#ifndef min
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#endif
+
 ssize_t ASOCKET__recv(asocket_t* socket, void* data, size_t size) {
-    return 0;
+    getchar();
+    memcpy(data, socket->buffer, min(size, socket->index + 1));
+    return min(size, socket->index + 1);
 }
 
--- src/main.c
+++ src/main.c
@@ -1,3 +1,7 @@
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
 #include "logger.h"
 #include "asocket/asocket.h"
 
@@ -19,7 +23,8 @@ int main(int argc, char **argv) {
     }
 
     LOG_INFO("Sending");
-    status = ASOCKET__send(socket, "Hello, World!", 14);
+    char* p = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries";
+    status = ASOCKET__send(socket, p, strlen(p));
     if (status != 0) {
         LOG_ERROR("Failed to send message on socket");
         goto l_cleanup;
@@ -32,6 +37,12 @@ int main(int argc, char **argv) {
         status  = -1;
         goto l_cleanup;
     }
+
+    LOG_DEBUG("got %zd", recv_length);
+    int fd = open("out.txt", O_CREAT | O_TRUNC|O_RDWR, S_IRWXU);
+    int out = write(fd, buffer, recv_length);
+    LOG_DEBUG("write: %d, %s", out, out == -1 ? strerror(errno) : "");
+    close(fd);
 
     LOG_INFO("Finished");
     status = 0;
